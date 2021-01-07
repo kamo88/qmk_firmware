72b3d37e257139323d172e886cb35b9183b46188
kamo88 <62532951+kamo88@users.noreply.github.com>
2021-01-07 19:13:23 +0900

    setting keymap

--- keyboards/amatelus73/keymaps/kamo88/keymap.c
+++ keyboards/amatelus73/keymaps/kamo88/keymap.c
@@ -23,8 +23,8 @@ enum layer_names {
     _ADJUST
 };
 
-#define RAISE LT(_RAISE, KC_ESC)
-#define LOWER LT(_LOWER, KC_TAB)
+#define RAISE LT(_RAISE, KC_SPC)
+#define LOWER LT(_LOWER, KC_SPC)
 #define SPACE RSFT_T(KC_SPC)
 #define ENTER RCTL_T(KC_ENT)
 
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base
     [_BASE] = LAYOUT(
       KC_PGUP, KC_ESC, KC_1,  KC_2,  KC_3,  KC_4,  KC_5, KC_MINS,    KC_GRV,   KC_EQL, KC_6,  KC_7,  KC_8,    KC_9,   KC_0,    KC_BSPC,
-      KC_PGDN,  KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_LBRC,       KC_RBRC,  KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,  KC_PIPE, KC_DEL,
+      KC_PGDN,  KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_LBRC,       KC_RBRC,  KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,  KC_BSLS, KC_DEL,
               KC_CAPS,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_BSPC,         KC_QUOT,  KC_H,  KC_J,  KC_K,    KC_L,   KC_SCLN,    KC_ENT,
           KC_LSFT,     KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_LPRN, A(KC_F4), KC_RPRN,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_UP,  KC_RSFT,
       KC_PDOT, KC_RCTRL,           ALT_T(KC_BSPC), SPACE,   RAISE,           LOWER,   ENTER, KC_LGUI,                KC_LEFT, KC_DOWN, KC_RGHT
